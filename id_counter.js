1673d33ba14195bebce95a7eb4c471e89fe52c04
andrewjumperdev <andrew.jumper.ent@gmail.com>
2024-03-08 14:15:50 +0100

    id-seq

--- id_counter.js
+++ id_counter.js
@@ -1,36 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-let idShortCounter = 0;
 const counterFilePath = path.join(__dirname, 'id_counter.json');
 
 function loadIdCounter() {
-    try {
-        if (fs.existsSync(counterFilePath)) {
-            const data = fs.readFileSync(counterFilePath, 'utf8');
-            const counterData = JSON.parse(data);
-            if (counterData && typeof counterData.idShortCounter === 'number') {
-                idShortCounter = counterData.idShortCounter;
-            }
-        }
-    } catch (error) {
-        console.error('Error loading ID counter:', error);
+    if (fs.existsSync(counterFilePath)) {
+        const counterData = JSON.parse(fs.readFileSync(counterFilePath));
+        idShortCounter = counterData.idShortCounter || 0;
+    } else {
+        idShortCounter = 0;
     }
 }
 
 function saveIdCounter() {
-    try {
-        const data = JSON.stringify({ idShortCounter });
-        fs.writeFileSync(counterFilePath, data);
-    } catch (error) {
-        console.error('Error saving ID counter:', error);
-    }
+    const counterData = { idShortCounter };
+    fs.writeFileSync(counterFilePath, JSON.stringify(counterData, null, 2));
 }
 
-function generateSequentialShortId() {
-    const id = String(++idShortCounter).padStart(8, '0');
-    console.log('Generated ID:', id);
-    return id.toUpperCase();
-}
-
-module.exports = { generateSequentialShortId, loadIdCounter, saveIdCounter };
+module.exports = { loadIdCounter, saveIdCounter };
